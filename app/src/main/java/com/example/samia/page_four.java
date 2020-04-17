7c719b9ab7aaea3e2a0fb3938ae247de1a3f6c16
Kiran <kirankrishna369@gmail.com>
2020-04-17 09:51:39 +0000

    samia

---
+++ app/src/main/java/com/example/samia/page_four.java
@@ -0,0 +1,14 @@
+package com.example.samia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class page_four extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_four);
+    }
+}
