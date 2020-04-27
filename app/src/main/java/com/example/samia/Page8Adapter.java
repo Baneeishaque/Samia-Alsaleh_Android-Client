4392fc2ce18a249675fac0db06f5cf1c7522f9e0
Kiran <kirankrishna369@gmail.com>
2020-04-28 00:58:50 +0530

    page 7,8 recyclerview

---
+++ app/src/main/java/com/example/samia/Page8Adapter.java
@@ -0,0 +1,64 @@
+package com.example.samia;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ndk.utils_android1.ActivityUtils;
+
+public class Page8Adapter extends RecyclerView.Adapter<Page8Adapter.MyHolder> {
+
+    List<String> categoryList = new ArrayList<>();
+
+    public Page8Adapter (List<String> categoryList){
+
+        this.categoryList = categoryList;
+
+    }
+
+    @NonNull
+    @Override
+    public Page8Adapter.MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.page_seven_button,parent,false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+        holder.buttonCare.setText(categoryList.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryList.size();
+    }
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+
+        private Button buttonCare;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ActivityUtils.startActivity(v.getContext(),page_nine.class);
+                }
+            });
+
+            buttonCare = (Button) itemView.findViewById(R.id.buttonCare);
+
+        }
+    }
+}
--- app/src/main/java/com/example/samia/page_seven.java
+++ app/src/main/java/com/example/samia/page_seven.java
@@ -6,6 +6,11 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import ndk.utils_android1.ActivityUtils;
 
@@ -13,20 +18,32 @@ public class page_seven extends AppCompatActivity {
 
     Context activityContext = this;
 
+    RecyclerView recyclerView;
+
+    List<String> categoryList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_seven);
 
-        Button buttonHairCare = findViewById(R.id.buttonHairCare);
-        buttonHairCare.setOnClickListener(new View.OnClickListener() {
+        initialize();
 
-            @Override
-            public void onClick(View v) {
+    }
 
-                ActivityUtils.startActivity(activityContext, page_eight.class);
-            }
-        });
+    private void initialize() {
+
+        recyclerView =(RecyclerView) findViewById(R.id.recycerView7);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        categoryList.add("Hair Care");
+        categoryList.add("BodyCare");
+        categoryList.add("SkinList");
+
+        recyclerView.setAdapter(new Page7Adapter(categoryList));
+
     }
 }
